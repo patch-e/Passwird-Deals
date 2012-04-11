804c08d53cfd7cbc3aedb0c99e33eac946c85980
Patrick Crager <p.crager@gmail.com>
2012-04-10 23:32:53 -0400

    Broke out the HTML used to show the deal in the WebView into a file and then using StringWithContentsOfFile to pull in the HTML

--- Passwird Deals/DetailViewController.m
+++ Passwird Deals/DetailViewController.m
@@ -69,61 +69,25 @@
         [dateFormatter setDateStyle:NSDateFormatterShortStyle];
         [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
     }
-    NSString *dateString = [dateFormatter stringFromDate:self.detailItem.datePosted];
+    NSString *dateAsString = [dateFormatter stringFromDate:self.detailItem.datePosted];
     
     // Update the user interface for the detail item.
     if (self.detailItem) {
+        NSError *error;
+        NSStringEncoding encoding;
+        NSString *dealHtmlFilePath = [[NSBundle mainBundle] pathForResource: @"Deal" 
+                                                                     ofType: @"html"];
+        NSString *dealHtmlString = [NSString stringWithContentsOfFile:dealHtmlFilePath 
+                                                   usedEncoding:&encoding 
+                                                          error:&error];
         
-        NSString *html = [NSString stringWithFormat:
-                          @"<html>"
-                          "<head>"
-                          "<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />"
-                          "<style type=\"text/css\">"
-                          "body {"
-                          "background-color: #ababab;"
-                          "font-family: Helvetica;"
-                          "padding-bottom: 5px;"
-                          "}"
-                          
-                          "a {"
-                          "color: #004875;"
-                          "-webkit-tap-highlight-color: rgba(255, 0, 0, 0.5);"
-                          "}"
-                          
-                          ".headline {"
-                          "margin: 0px;"      
-                          "padding: 0px 10px;"
-                          "font-size: 18px;"
-                          "color: #000;"
-                          "text-overflow: inherit;"
-                          "white-space: normal;"
-                          "overflow: visible;"
-                          "}"
-                          
-                          ".body {"
-                          "background-color: #fff;"
-                          "border: 1px solid #ADAAAD;"
-                          "margin: 10px 5px 10px 5px;"
-                          "padding: 8px;"
-                          "font-size: 16px;"
-                          "-webkit-border-radius: 8px;"
-                          "-webkit-box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);"
-                          "}"
-                          
-                          ".date { float: right; margin-left: 5px; }"
-                          ".expired { color: #f90602; }"
-                          ".center { text-align: center; }"
+        NSString *html = [NSString stringWithFormat:dealHtmlString, 
+                            dateAsString, 
+                            self.detailItem.headline, 
+                            (self.detailItem.isExpired ? @"(expired)" : @""), 
+                            self.detailItem.imageURL, 
+                            self.detailItem.body];
 
-                          "</style>"
-                          "</head>"
-                          "<body>"
-                          "<small class=\"date\">%@</small>"
-                          "<h1 class=\"headline\">%@ <span class=\"expired\">%@</span></h1>"
-                          "<div class=\"body center\"><img src=\"%@\"/></div>"
-                          "<div class=\"body\">%@</div>"
-                          "</body>"
-                          "</html>", dateString, self.detailItem.headline, (self.detailItem.isExpired ? @"(expired)" : @""), self.detailItem.imageURL, self.detailItem.body];
-        
         [self.webView loadHTMLString:html baseURL:nil];
     }
 }
@@ -143,24 +107,24 @@
     [self.activityIndicator stopAnimating];
     
     if ( [[[self.webView.request URL] absoluteString] isEqualToString:@"about:blank"] ) {
-        self.dealButton.enabled = NO; 
-        self.safariButton.enabled = NO;    
-        self.backButton.enabled = NO;
-        self.forwardButton.enabled = NO;
+        [self.dealButton setEnabled:NO]; 
+        [self.safariButton setEnabled:NO];    
+        [self.backButton setEnabled:NO];
+        [self.forwardButton setEnabled:NO];
     }
     else {
-        self.dealButton.enabled = YES; 
-        self.safariButton.enabled = YES;
+        [self.dealButton setEnabled:YES]; 
+        [self.safariButton setEnabled:YES];
         
         if ([self.webView canGoBack])
-            self.backButton.enabled = YES;
+            [self.backButton setEnabled:YES];
         else
-            self.backButton.enabled = NO;  
+            [self.backButton setEnabled:NO];  
         
         if ([self.webView canGoForward])
-            self.forwardButton.enabled = YES;   
+            [self.forwardButton setEnabled:YES];   
         else
-            self.forwardButton.enabled = NO;      
+            [self.forwardButton setEnabled:NO];      
     }
 }
 
