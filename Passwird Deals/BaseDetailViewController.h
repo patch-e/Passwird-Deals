0f57db6c428dc2c5baab8fd5bb71caec34b4f346
Patrick Crager <p.crager@gmail.com>
2012-09-13 06:44:42 -0400

    ?

---
+++ Passwird Deals/BaseDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseDetailViewController.h
+//  Passwird Deals
+//
+//  Created by Patrick Crager on 8/12/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseDetailViewController : UIViewController
+
+@end
